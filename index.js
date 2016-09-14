2e1e8193e7458d81f11289ef86b564f07709de2b
Kalbir Sohi <kalbir@gmail.com>
2016-09-14 23:05:46 +0100

    Made it more html like

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
-fs.readFile('instructions.html', 'utf8', function (err,data) {
+fs.readFile('howto.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
