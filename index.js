c29ca073453b1758c2359deb1b310c4bfafd068f
Kalbir Sohi <kalbir@gmail.com>
2016-09-14 22:06:52 +0100

    Testing a change on drie

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+fs.readFile('instructions', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  app.set('port', (process.env.PORT || 5000))
+  app.use(express.static(__dirname + '/public'))
+
+  app.get('/', function(request, response) {
+    response.send(data);
+  })
+
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  })
+
+  console.log(data);
+});
