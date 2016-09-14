3e58f0587030d9fb3cd85fbe46f570b62119b638
Kalbir Sohi <kalbir@gmail.com>
2016-09-14 23:57:32 +0100

    Add a route to find the useragent

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ var express = require('express')
 var app = express()
 var fs = require('fs')
 var markdown = require( "markdown" ).markdown
+var useragent = require('express-useragent');
 
 fs.readFile('howto', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
 
+  app.use(useragent.express());
   app.set('port', (process.env.PORT || 5000))
   app.use(express.static(__dirname + '/public'))
 
   app.get('/', function(request, response) {
     response.send(markdown.toHTML(data.toString()));
   })
+
+  app.get('/useragent', function(req, res){
+          res.send(req.useragent);
+  });
 
   app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
