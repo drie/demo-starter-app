c662db2bf90e8a9b526b410f638e5dc7b9ad3b0c
Kalbir Sohi <kalbir@gmail.com>
2016-10-19 08:23:26 +0100

    Making the instructions more simple

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
-var markdown = require( "markdown" ).markdown
-var useragent = require('express-useragent');
 
-fs.readFile('howto', 'utf8', function (err,data) {
+fs.readFile('content', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
 
-  app.use(useragent.express());
   app.set('port', (process.env.PORT || 5000))
   app.use(express.static(__dirname + '/public'))
 
   app.use(function(req, res, next) {
-    if (req.useragent.isCurl === true)
 	  app.get('/', function(request, response) {
        response.send(data);
-      });
-   else
-      app.get('/', function(request, response) {
-        response.send(markdown.toHTML(data.toString()));
       });
     next();
   });
  
-  app.get('/useragent', function(req, res){
-    res.send(req.useragent);
-  });
-
   app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port')) 
   })
